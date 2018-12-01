d38dc00b59e868ab486c124802353148c2ff6ab5
shantamshrestha <shantamshrestha@gmail.com>
2018-12-01 08:31:12 +0545

    jQuery intergration for AJAX calls

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,7 +1,52 @@
+// $('btnLogin').click(function(){
+//     $.post("Hello",
+//         {
+//             username:"Shantam",
+//             password:"password"
+//         },
+//         function(data,status){
+//             alert("Data: " +  data + "\nStatus" + status);
+//         });
+// });
+
 function Login(){
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
+    console.log("here")
+    $.post("Hello",
+        {
+            username:"Shantam",
+            password:"password"
+        },
+        function(data,status){
+            alert("Data: " +  data + "\nStatus" + status);
+        });
+}
 
-    
+// $('#btnLogin').click(function(){
+//     $.post("Hello",
+//         {
+//             username:"Shantam",
+//             password:"password"
+//         },
+//         function(data,status){
+//             alert("Data: " +  data + "\nStatus" + status);
+//         });
+// });
 
-};
+$(document).ready(function(){
+    alert("Hello");
+});
+
+alert("hello")
+// $(document).ready(function(){
+//     $(".table").hover(function(){
+//         alert("Hovering");
+//     },
+//     function(){
+//         alert("Hovering done");
+//     });
+// });
+
+$("#username").click(function(){
+    $("#username").hide();
+});
+alert("hello")
