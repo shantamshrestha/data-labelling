8fbbc062fbb2f711c38a716fe15939defe8168a2
shantamshrestha <shantamshrestha@gmail.com>
2018-12-01 08:26:56 +0545

    CORS policy added

--- Backend/main/__init__.py
+++ Backend/main/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask,Blueprint,request
+from flask_cors import CORS
 import MySQLdb as SQL
 import jwt
 app = Flask(__name__)
+CORS(app)
 app.config['SECRET_KEY'] = 'letitbe'
 
 from main.controller.login import auth
